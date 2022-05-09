ca10363a3af7191c381b3006dda95679a57b228a
Yocheved Indik <chevedindik@gmail.com>
2022-05-09 18:06:30 -0400

    changes

--- ReactPeopleBackend.Web/ClientApp/src/AddPersonForm.js
+++ ReactPeopleBackend.Web/ClientApp/src/AddPersonForm.js
@@ -1,25 +1,6 @@
 import React from 'react';
 
-// class AddPersonForm extends React.Component{
 
-// render(){
-// const { firstName, lastName, age, onTextChange, onAddButtonClicked } = this.props  
-//     return <div className="row jumbotron">
-//         <div className="col-md-3">
-//             <input value={firstName} onChange={onTextChange} name='firstName' type="text" className="form-control" placeholder="First Name" />
-//         </div>
-//         <div className="col-md-3">
-//             <input value={lastName} onChange={onTextChange} name='lastName' type="text" className="form-control" placeholder="Last Name" />
-//         </div>
-//         <div className="col-md-3">
-//             <input value={age} onChange={onTextChange} name='age' type="text" className="form-control" placeholder="Age" />
-//         </div>
-//         <div className="col-md-3">
-//             <button className='btn btn-primary btn-block'  onClick={onAddButtonClicked}>Add</button>
-//         </div>
-//     </div>
-// }
-// }
 
 
 function AddPersonForm({person, onTextChange, onAddButtonClicked, onCancelClick, onUpdateClick, editing}){
